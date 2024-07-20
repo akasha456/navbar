051ae52eaca0e2e2d13b979deaa1491a22fe5ae0
Akash <akashanil456@gmail.com>
2024-07-20 13:19:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Navbar from './Component/Navbar'
+
+const App = () => {
+  return (
+      <div className='container'>
+        <Navbar/>
+      </div>  
+  )
+}
+
+export default App
